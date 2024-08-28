e89f5f72fb99dbbe57b98c9bf2537e41b3e1fb1a
Sunil Sattiraju <sunilsattiraju@gmail.com>
2024-08-28 10:27:57 +0800

    check semantic kernel version installed

--- web_researcher/flows/experiment/summarize_pdf_content.py
+++ web_researcher/flows/experiment/summarize_pdf_content.py
@@ -1,13 +1,21 @@
 import asyncio
 import os
-
+from importlib.metadata import version
 from promptflow.core import tool
 from promptflow.connections import AzureOpenAIConnection
-from semantic_kernel.contents.chat_message_content import ChatMessageContent
-from semantic_kernel.contents.utils.author_role import AuthorRole
-from semantic_kernel.kernel import Kernel
-from semantic_kernel.agents.open_ai.azure_assistant_agent import AzureAssistantAgent
 
+sk_version = version("semantic_kernel")
+print("Semantic Kernel version:", sk_version)
+if sk_version == "1.8.0":
+    from semantic_kernel.contents.chat_message_content import ChatMessageContent
+    from semantic_kernel.contents.utils.author_role import AuthorRole
+    from semantic_kernel.kernel import Kernel
+    from semantic_kernel.agents.open_ai.azure_assistant_agent import AzureAssistantAgent
+else:
+    from semantic_kernel.contents import ChatMessageContent
+    from semantic_kernel.contents import AuthorRole
+    from semantic_kernel.kernel import Kernel
+    from semantic_kernel.agents import AzureAssistantAgent
 # import nest_asyncio
 
 # nest_asyncio.apply()  # Required for Jupyter Notebooks asyncio.run() cannot be called from a running event loop
