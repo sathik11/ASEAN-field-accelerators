e1510a952e8f54a0694cef691448824d458d4728
Sunil Sattiraju <sunilsattiraju@gmail.com>
2024-09-11 18:49:07 +0800

    fix config issues

--- frontend/genai-showcase/next.config.mjs
+++ frontend/genai-showcase/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  reactStrictMode: true,
+  swcMinify: true,
+  // Ensure that Next.js can handle static assets correctly
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
