da8f6a7d749a142977c216cfdda7a4859fcc76a7
Sunil Sattiraju <sunilsattiraju@gmail.com>
2024-09-11 19:06:03 +0800

    update config for swa

--- frontend/genai-showcase/tailwind.config.ts
+++ frontend/genai-showcase/tailwind.config.ts
@@ -6,7 +6,9 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
     extend: {
       colors: {
