a2414cda95819ffec481320bf003c496b9b5068c
Sunil Sattiraju <sunilsattiraju@gmail.com>
2024-09-15 12:17:48 +0800

    clean up swa

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  reactStrictMode: true,
-  swcMinify: true,
   // Ensure that Next.js can handle static assets correctly
   images: {
     unoptimized: true,
